221fdd568787a3381e56da0db06707e95f59d9c1
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2023-11-19 13:24:02 +0100

    Terminado ej de práctica 3

--- Map/map_3.cpp
+++ Map/map_3.cpp
@@ -10,6 +10,7 @@
 #include <map>
 #include <set>
 #include <list>
+#include <algorithm>
 using namespace std;
 
 int main(){
@@ -26,10 +27,37 @@ int main(){
         }
     } while(palabra != "0");
 
+    cout << "\n\n"; 
+
+    cout << "La lista generada es la siguiente:\n";
     for (auto it = lista_palabras.begin(); it != lista_palabras.end(); ++it){
         cout << *it << " ";
     }
+    cout << "\n\n";
+
+    for (const auto palabra : lista_palabras){
+        string palabra_ordenada = palabra;
+        sort(palabra_ordenada.begin(), palabra_ordenada.end());
+
+        if (palabra_ordenada.size() != 1)
+            anagr[palabra_ordenada].emplace(palabra);
+    }
+
+    for (auto it = anagr.begin(); it != anagr.end(); ++it){
+        cout << it->first << ":";
+        for (const auto i : it->second){
+            cout << i << " ";
+        }
+        if (it->second.size() == 1){
+                cout << " ----> No existen anagramas";
+        }else{
+            cout << " ----> Existen " << it->second.size() << " anagramas";
+        }
+        cout << endl;
+    }
     cout << endl;
 
+
+    
 
 }
